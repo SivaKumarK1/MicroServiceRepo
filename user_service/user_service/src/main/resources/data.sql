c1bbc801792a3c98db5d9953a8d25dfceece46fb
Anil Konda <anilkumar.k@comakeit.com>
2022-03-25 12:08:10 +0530

    properties added

--- user_service/user_service/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO USER (user_id, name, phone) VALUES (1311, 'Anil Kumar', '23525625');
-INSERT INTO USER (user_id, name, phone) VALUES (1312, 'Siva Kumar', '95896412');
-INSERT INTO USER (user_id, name, phone) VALUES (1314, 'Rakesh Kumar', '87456231');
