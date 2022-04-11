2acd3060ac4d4cab2de3baf370ddc02f5666c870
Anil Comakeit <anilkumar.k@comakeit.com>
2022-04-11 11:27:34 +0000

    Update .gitlab-ci.yml, Terraform/Project/make.sh

--- Terraform/Project/make.sh
+++ Terraform/Project/make.sh
@@ -12,20 +12,22 @@ terraform apply  --auto-approve
 terraform output | grep -o '(\d+[.]){3}\d+' > ips
 
 #change master ip in hosts file
-# rm temp.txt
-# sed -i "s/$(awk 'FNR == 2' inventory/hosts > temp.txt && awk '{print $1}' temp.txt)/$(awk "FNR == 2" ips)/g" inventory/hosts
-#change worker ip in hosts file
-# rm temp.txt
-# sed -i "s/$(awk 'FNR == 4' inventory/hosts > temp.txt && awk '{print $1}' temp.txt)/$(awk "FNR == 3" ips)/g" inventory/hosts
-#change postgres ip in hosts file
-# rm temp.txt
-# sed -i "s/$(awk 'FNR == 6' inventory/hosts > temp.txt && awk '{print $1}' temp.txt)/$(awk "FNR == 1" ips)/g" inventory/hosts
+rm temp.txt
+sed -i "s/$(awk 'FNR == 2' inventory/hosts > temp.txt && awk '{print $1}' temp.txt)/$(awk "FNR == 2" ips)/g" inventory/hosts
+change worker ip in hosts file
+rm temp.txt
+sed -i "s/$(awk 'FNR == 4' inventory/hosts > temp.txt && awk '{print $1}' temp.txt)/$(awk "FNR == 3" ips)/g" inventory/hosts
+change postgres ip in hosts file
+rm temp.txt
+sed -i "s/$(awk 'FNR == 6' inventory/hosts > temp.txt && awk '{print $1}' temp.txt)/$(awk "FNR == 1" ips)/g" inventory/hosts
 
 
-# sleep 1m
+sleep 1m
 # # Running ansible
 # export ANSIBLE_HOST_KEY_CHECKING=False
 
 # ansible-playbook -i inventory/hosts playbook.yml
 
-# rm ips  temp.txt
+rm ips  temp.txt
+
+cat inventory/hosts
