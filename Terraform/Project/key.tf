115f1f0a2c1ba84c2363f9c62c26c0d409a6c55a
SivaKumarK1 <sivakumar.k@comakeit.com>
2022-04-11 06:52:10 +0000

    Update Terraform/Project/main.tf, Terraform/Project/key.tf

--- Terraform/Project/key.tf
+++ Terraform/Project/key.tf
@@ -1,14 +1,14 @@
-variable "access_key" {
-  description = "Access Key"
-  type = string
-  sensitive = true
-}
+// variable "access_key" {
+//   description = "Access Key"
+//   type = string
+//   sensitive = true
+// }
 
-variable "secret_key" {
-  description = "Secret Key"
-  type = string
-  sensitive = true
-}
+// variable "secret_key" {
+//   description = "Secret Key"
+//   type = string
+//   sensitive = true
+// }
 
 variable "bootstrap_path" {
   description = "Script to install Docker Engine"
